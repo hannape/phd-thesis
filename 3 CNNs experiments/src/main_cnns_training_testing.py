ef50d2703d008257e5b89f0da9e82759e71a3633
hannape <44372321+hannape@users.noreply.github.com>
2022-06-02 18:36:17 +0200

    cnns experiments
    architecture + hyperparameter optimization, trying to find the best model for bird calls detection

---
+++ 3 CNNs experiments/src/main_cnns_training_testing.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pickle
+import time
+import sys
+import pandas as pd
+import numpy as np
+from keras import backend as K
+K.set_image_data_format('channels_last')
+from numpy.random import seed
+import tensorflow as tf
+from tensorflow.compat.v1.keras.backend import set_session
+from sklearn.metrics import average_precision_score, roc_auc_score 
+from sklearn.metrics import precision_recall_curve, roc_curve
+import matplotlib.pyplot as plt
+
+seed(1)
+tf.random.set_seed(2)
+
+import config
+from saving_utils import LoggerWrapper 
+from useful_func_cnn import load_my_data, load_my_data_multitap, make_cnn_grid_corr, load_test, load_test_multitap
+
+# allocating fraction of GPU memory
+config2 = tf.compat.v1.ConfigProto()
+config2.gpu_options.per_process_gpu_memory_fraction = 0.5 
+set_session(tf.compat.v1.Session(config=config2))
+
+''' Training and testing models with parameters chosen by random search. 
+    Saving models, creating predictions for test set, and drawing PR and ROC plots.
+    
+    Running script: python script_name representation_type directory_with_cnn_random_search_results which_model_checked(1st,2nd,3rd best) max_number_of_epochs number_of_models_trained 
+     e.g. python main_testing_cnns.py mel-spektrogram ..\results\randomSearch\mel-spektrogram\2021-05-26-22-58\ 1 100 5
+'''
+
+# %%
+
+if __name__=='__main__':
+
+    # Load configs
+    chosen_repr = sys.argv[1]
+    with open(os.path.join(sys.argv[2],'all_models.pkl'), 'rb') as f:
+        all_models = pickle.load(f)
+        
+    with open(os.path.join(sys.argv[2],'all_models_scores.pkl'), 'rb') as g:    
+        all_models_scores = pickle.load(g)
+        
+    # list of indices of 3 best models
+    best_Nth_idx = list((all_models_scores.sort_values(by=[0], ascending=False).nlargest(int(sys.argv[3] ), columns = 0)).index)[-1]
+    best_params = all_models.iloc[best_Nth_idx]
+    print(best_params)
+    
+    epochs = int(sys.argv[4] )
+    n_models = int(sys.argv[5] )
+    exp_dir = os.path.join("..","results","PR", f"{chosen_repr}", time.strftime('%Y-%m-%d-%H-%M'))
+    try:
+        os.makedirs(exp_dir)
+    except FileExistsError:
+        pass
+    
+    start_time = time.time()
+    logger_wrapper = LoggerWrapper(exp_dir)
+    sys.stderr.write = logger_wrapper.log_errors
+    logger_wrapper.logger.info(f'Running {sys.argv}')
+    
+    if chosen_repr=="multitaper": 
+        cv_split, dX, dy, di, cv, X_val, y_val, train_X, train_y = load_my_data_multitap(chosen_repr, config) 
+    else:
+        cv_split, dX, dy, di, dX_test, dy_test, di_test, cv, X_val, y_val, train_X, train_y = load_my_data(chosen_repr, config) 
+    
+    print(chosen_repr)
+    if chosen_repr=="spektrogram":
+        in_shape = (63, 148, 1)
+    if chosen_repr=="mel-spektrogram":
+        in_shape = (60, 148, 1)    
+    if chosen_repr=="multitaper":
+        in_shape = (64, 149, 1)  
+           
+    dX_resh = dX.reshape(dX.shape[0], in_shape[0], in_shape[1], 1).astype('float32')     
+    input_shape = (np.shape(dX_resh)[1], np.shape(dX_resh)[2], np.shape(dX_resh)[3])
+    
+    # read test set
+    if chosen_repr=="multitaper":
+        testing_features, testing_target, testy_identifiers = load_test_multitap(chosen_repr)  
+        testing_features_resh = testing_features.reshape(testing_features.shape[0], in_shape[0], in_shape[1], 1).astype('float32')
+    else:         
+        testing_features, testing_target = load_test(chosen_repr)     
+        testing_features_resh = testing_features.reshape(testing_features.shape[0], in_shape[0], in_shape[1], 1).astype('float32')
+    
+    print("testing target ", np.shape(testing_target))
+    print("testing features ", np.shape(testing_features))
+
+    print("Fit model on training data")
+    y_val = y_val.astype(int)
+    train_y = train_y.astype(int)
+    
+    X_val_resh = X_val.reshape(X_val.shape[0],in_shape[0], in_shape[1], 1).astype('float32') 
+    train_X_resh = train_X.reshape(train_X.shape[0], in_shape[0], in_shape[1], 1).astype('float32')
+    
+    my_callbacks = [ tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=5, restore_best_weights = True), ]
+
+    best_N_idx = list((all_models_scores.sort_values(by=[0], ascending=False).nlargest(int(sys.argv[3] ), columns = 0)).index)
+    names= ["1st-best", "2nd-best","3rd-best"]
+    n_plots = 0
+    
+    for num, k in enumerate(best_N_idx):
+        
+        best_params = all_models.iloc[k]
+        logger_wrapper.logger.info('Model')
+        logger_wrapper.logger.info(names[num])
+        logger_wrapper.logger.info('chosen parameters to test:')
+        logger_wrapper.logger.info(best_params)
+        logger_wrapper.logger.info('Index of best model')
+        logger_wrapper.logger.info(k)
+        
+        exp_dir_sub = os.path.join(exp_dir,"model-" + names[num])
+        try:
+            os.makedirs(exp_dir_sub)
+        except FileExistsError:
+            pass
+        preds_data, aucroc_data, aucpr_data = [], [], []
+        
+        
+        for n in range (0,n_models):
+            print("*************  Run ", n, ", model ", num," with index ", k, "  ********************")
+            model_refit = make_cnn_grid_corr(input_shape = best_params['input_shape'],
+                                lr = best_params['lr'],
+                                filters = best_params['filters'],
+                                drop_out = best_params['drop_out'],
+                                layers = best_params['layers'],
+                                if_dropout = best_params['if_dropout'],
+                                dense_layer_sizes = best_params['dense_layer_sizes']
+                                )
+        
+            hist = model_refit.fit(train_X_resh, train_y.astype(np.float32), validation_data=(X_val_resh, y_val.astype(np.float32)), callbacks = my_callbacks,
+                                           epochs=epochs, batch_size=best_params['batch_size']) 
+
+            # save model
+            model_refit.save(os.path.join(exp_dir_sub,"my-model"+str(n)+".h5"))
+            print("Saved model to disk")       
+         
+            y_preds = model_refit.predict_proba(testing_features_resh) 
+            
+            preds_data.append(y_preds)       
+            prec_nb, recall_nb, _ = precision_recall_curve(testing_target, y_preds)
+            fpr_nb, tpr_nb, _ = roc_curve(testing_target, y_preds)
+            print('AUC PR: ', average_precision_score(testing_target, y_preds))
+
+            aucpr_data.append(average_precision_score(testing_target, y_preds))
+            aucroc_data.append(roc_auc_score(testing_target, y_preds))
+    
+            fig = plt.figure(figsize=(10,10))
+            plt.rcParams.update({'font.size': 22})
+            plt.title('AUC PR')
+            plt.plot(recall_nb, prec_nb,  'b', label = 'CNN AUC = %0.2f%% ' % (100*average_precision_score(testing_target, y_preds)))
+            plt.legend(loc = 'upper right')
+            plt.xlim([0, 1])
+            plt.ylim([0, 1])
+            plt.ylabel('Precyzja (Precision)')
+            plt.xlabel('Czułość (Recall)')
+            fig.savefig(os.path.join(exp_dir_sub,'AUC-PR-refit-'+ str(n) + time.strftime('-%Y-%m-%d-%H-%M')))
+            
+            fig = plt.figure(figsize=(10,10))
+            plt.rcParams.update({'font.size': 22})
+            plt.title('AUC ROC')
+            plt.plot(fpr_nb, tpr_nb, 'b', label = 'CNN AUC = %0.2f%%' % (100*roc_auc_score(testing_target, y_preds)))
+            plt.legend(loc = 'lower right')
+            plt.plot([0, 1], [0, 1],'r--')
+            plt.xlim([0, 1])
+            plt.ylim([0, 1])
+            plt.ylabel('Wskaźnik czułości (True Positive Rate)')
+            plt.xlabel('Odsetek fałszywie pozytywnych alarmów (False Positive Rate)')
+            fig.savefig(os.path.join(exp_dir_sub,'AUC-ROC-refit-'+ str(n) +time.strftime('-%Y-%m-%d-%H-%M')))
+          
+            # summarize history for loss
+            fig = plt.figure(figsize=(10,10))
+            plt.plot(hist.history['loss'])
+            plt.plot(hist.history['val_loss'])
+            plt.title('Model loss')
+            plt.ylabel('Koszt (loss)')
+            plt.xlabel('Liczba epok')
+            plt.legend(['trening','walid'], loc='upper right')
+            fig.savefig(os.path.join(exp_dir_sub,'uczenie-loss-refit-'+ str(n) +time.strftime('-%Y-%m-%d-%H-%M')))
+            
+            if n_plots==0:
+                auc_nr = 'auc'
+                val_auc_nr = 'val_auc'
+            else:
+                auc_nr = 'auc_'+str(n_plots)
+                val_auc_nr = 'val_auc_'+ str(n_plots)
+                
+            n_plots = n_plots + 1
+            
+            fig = plt.figure(figsize=(10,10))
+            plt.plot(hist.history[auc_nr])
+            plt.plot(hist.history[val_auc_nr])
+            plt.title('Model PR AUC')
+            plt.ylabel('Pole pod krzywą PR')
+            plt.xlabel('Liczba epok')
+            plt.legend(['trening','walid'], loc='lower right')
+            fig.savefig(os.path.join(exp_dir_sub,'uczenie-PR-AUC-refit-' + str(n) + time.strftime('-%Y-%m-%d-%H-%M')))
+            
+# %%  Printing & saving data for 5 models
+        print('PR AUC mean: ' , np.mean(aucpr_data), '+/- std', np.std(aucpr_data) )
+        print('ROC AUC mean: ' , np.mean(aucroc_data), '+/- std', np.std(aucroc_data) )
+        
+        logger_wrapper.logger.info('PR AUC mean i std:')
+        logger_wrapper.logger.info(np.mean(aucpr_data))
+        logger_wrapper.logger.info(np.std(aucpr_data))
+        logger_wrapper.logger.info('ROC AUC mean i std:')
+        logger_wrapper.logger.info(np.mean(aucroc_data))
+        logger_wrapper.logger.info(np.std(aucroc_data))    
+        
+        pd.DataFrame(np.transpose(np.squeeze(preds_data)), columns = ['preds-1','preds-2','preds-3','preds-4','preds-5']).to_pickle(os.path.join(exp_dir_sub,"5_runs_preds.pkl") )   
+        pd.DataFrame(aucpr_data).to_pickle(os.path.join(exp_dir_sub,"5_runs_aucpr.pkl") ) 
+        pd.DataFrame(aucroc_data).to_pickle(os.path.join(exp_dir_sub,"5_runs_aucroc.pkl") ) 
+        
+#%% ensemble of 5 models, plots and saving   
+        averaged_preds = pd.DataFrame(np.transpose(np.squeeze(preds_data))).mean(axis=1)
+        pd.DataFrame(averaged_preds).to_pickle(os.path.join(exp_dir_sub,"5_runs_averaged_preds.pkl") )
+        
+        prec_av, recall_av, _ = precision_recall_curve(testing_target, averaged_preds)
+        fpr_av, tpr_av, _ = roc_curve(testing_target, averaged_preds)
+        print('ensemble AUC PR: ', average_precision_score(testing_target, averaged_preds))
+        print('ensemble AUC ROC: ', roc_auc_score(testing_target, averaged_preds))
+        logger_wrapper.logger.info('PR AUC ensembled:')
+        logger_wrapper.logger.info(average_precision_score(testing_target, averaged_preds))
+        logger_wrapper.logger.info('ROC AUC ensembled:')
+        logger_wrapper.logger.info(roc_auc_score(testing_target, averaged_preds))
+    
+        fig = plt.figure(figsize=(10,10))
+        plt.rcParams.update({'font.size': 22})
+        plt.title('AUC PR')
+        plt.plot(recall_av, prec_av,  'b', label = 'CNN AUC = %0.2f%% ' % (100*average_precision_score(testing_target, averaged_preds)))
+        
+        plt.legend(loc = 'upper right')
+        plt.xlim([0, 1])
+        plt.ylim([0, 1])
+        plt.ylabel('Precyzja (Precision)')
+        plt.xlabel('Czułość (Recall)')
+        fig.savefig(os.path.join(exp_dir_sub,'AUC-PR-averaged5-' + time.strftime('-%Y-%m-%d-%H-%M')))
+
+        fig, ax = plt.subplots(figsize=(10,10))
+        plt.rcParams.update({'font.size': 22})
+        plt.title('AUC ROC')
+        plt.plot(fpr_av, tpr_av, 'b', label = 'CNN AUC = %0.2f%%' % (100*roc_auc_score(testing_target, averaged_preds)))
+            
+        plt.legend(loc = 'lower right')
+        plt.plot([0, 1], [0, 1],'r--')
+        plt.xlim([0, 1])
+        plt.ylim([0, 1])
+        plt.ylabel('Wskaźnik czułości (True Positive Rate)')
+        plt.xlabel('Odsetek fałszywie pozytywnych alarmów (False Positive Rate)')
+        fig.savefig(os.path.join(exp_dir_sub,'AUC-ROC-averaged5-'+time.strftime('-%Y-%m-%d-%H-%M')))
---
+++ 3 CNNs experiments/src/useful_func_cnn.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+
+import keras
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import os
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten 
+from keras.layers import Conv2D, MaxPooling2D, SpatialDropout2D
+from  sklearn.model_selection import PredefinedSplit
+
+
+def make_cnn_grid_corr(input_shape = (63, 148, 1),  lr=0.001, filters = 10, drop_out = 0.5, layers = 3,
+                  dense_layer_sizes=256, activation_function = 'relu', if_dropout = 0 ): 
+  
+    kernel_size = (3,3)
+    pool_size = (2,2)
+    
+    model = Sequential()
+    model.add(Conv2D(filters, kernel_size,input_shape=input_shape, activation=activation_function))
+    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(SpatialDropout2D(if_dropout))
+    
+    for i in range(layers-1):
+        model.add(Conv2D(filters, kernel_size, activation=activation_function))
+        model.add(MaxPooling2D(pool_size=pool_size))
+        model.add(SpatialDropout2D(if_dropout))
+       
+    model.add(Flatten())
+    model.add(Dense(dense_layer_sizes, activation=activation_function))
+    model.add(Dropout(drop_out))
+    model.add(Dense(32, activation=activation_function))
+    model.add(Dropout(drop_out))
+    model.add(Dense(1, activation='sigmoid'))
+    
+    model.compile(loss='binary_crossentropy',
+                  optimizer=keras.optimizers.Adam(learning_rate = lr),
+                  metrics=[tf.keras.metrics.AUC(curve='PR')])#  [av_precision]
+
+    return model
+
+def load_my_data(chosen_repr, config):
+    
+    df1 = pd.read_hdf(os.path.join('..','data','df_train_'+ str(chosen_repr) +'_norm.h5'),'df')
+    df2 = pd.read_hdf(os.path.join('..','data','df_valid_norm.h5'),'df')
+    
+    all_repr = config.representation_1d + config.representation_2d
+    all_repr.remove(chosen_repr)
+    df2 = df2.drop(all_repr, axis=1)
+    cv_split, dX, dy, di, cv = make_cv_split_cnn(df1, df2, chosen_repr)    
+    print("Loaded train + valid")
+    
+    valid = df2.loc[:,['rec_name', 'chunk_ids', chosen_repr, 'has_bird']].values
+    train = df1.loc[:,['rec_name', 'chunk_ids', chosen_repr, 'has_bird']].values
+    valid_X = np.stack(valid[:, 2])
+    valid_y = valid[:, 3]
+    train_X = np.stack(train[:, 2])
+    train_y = train[:, 3]
+   
+    return cv_split, dX, dy, di, cv, valid_X, valid_y, train_X, train_y
+
+def make_cv_split_cnn(train, valid, chosen_repr, classifier=False):   
+    
+    train = train.loc[:,['rec_name', 'chunk_ids', chosen_repr, 'has_bird']].values
+    train_identifiers = train[:, 0:2]
+    train_X = np.stack(train[:, 2])
+    train_y = train[:, 3]
+    
+    valid = valid.loc[:,['rec_name', 'chunk_ids', chosen_repr, 'has_bird']].values
+    valid_identifiers = valid[:, 0:2]
+    valid_X = np.stack(valid[:, 2])
+    valid_y = valid[:, 3]
+
+    dX = np.vstack((train_X, valid_X))
+    print('min train+walid: ', np.amin(dX))
+    dy = np.hstack((train_y, valid_y))
+    di = np.vstack((train_identifiers, valid_identifiers))
+    
+    train_indices = np.array(range(0, train_X.shape[0]))
+    val_indices = np.array(range(train_X.shape[0], dX.shape[0]))   
+    cv_split = [(train_indices, val_indices)]   
+    dX = np.reshape(dX, newshape=(dX.shape[0],-1))
+    dy = dy.astype(int)
+   
+    test_fold = np.concatenate([
+    np.full( train_X.shape[0],-1, dtype=np.int8),
+    np.zeros(valid_X.shape[0], dtype=np.int8)
+    ])
+    cv = PredefinedSplit(test_fold)
+
+    return cv_split, dX, dy, di, cv
+
+
+def load_test(chosen_repr):
+    import config as cfg
+    from data_load import function_data_load
+    from representations_all import function_representations_all
+    import numpy as np
+    import time 
+
+    if_scaler = True                   # if the scaler is used on testing data.
+    nr_rec_start = 0                   # starting rec number, needed if we want to read many long recordings
+    rec_batch_size = 18                # recording reading batch size
+    test_new_only = 1                  # only test set is loaded 
+    
+    # DATA LOAD: Read data from recordings, read labels, create chunks, create testing set
+    
+    start_time = time.time()
+    
+    _,_,_, data_test_new = function_data_load(cfg.path_test1618_txt, cfg.path_train161718_txt, cfg.path_test1618_wav,\
+                                              cfg.path_train161718_wav, cfg.balance_types, cfg.balance_ratios, cfg.chunk_length_ms,\
+                                              cfg.chunk_overlap, cfg.calls_0, cfg.calls_1, cfg.calls_unknown, cfg.tolerance, \
+                                              cfg.valid_set, cfg.test_rec_to_cut, cfg.columns_dataframe, test_new_only)
+    print("--- Data load:: %s sec ---" % (time.time() - start_time))
+    
+    # REPRESENTATIONS: Create representations for test set
+    
+    start_time = time.time()
+    
+    file_names, indices, info_chunksy, repr_full_scaled = function_representations_all(cfg.path_test1618_wav, data_test_new, if_scaler, \
+                                                       cfg.repr_1d_summary, cfg.summary_1d, cfg.sr, cfg.chunk_length_ms, \
+                                                       cfg.n_fft, cfg.win_length, cfg.hop_length, cfg.window, cfg.f_min, cfg.f_max,\
+                                                       cfg.n_mels, cfg.N, cfg.step, cfg.Np, cfg.K, cfg.tm, cfg.flock, cfg.tlock,\
+                                                       chosen_repr, nr_rec_start, rec_batch_size)    
+        
+    print("--- Data representation: %s min ---" % ((time.time() - start_time)/60))
+
+    file_names_list, chunk_ids_list, has_bird_list, representation_list = [],[],[],[]
+    
+    
+    for num,i in enumerate(indices[nr_rec_start:nr_rec_start+rec_batch_size]):    
+      file_names_list.extend([file_names[num] for i in range(len(i))])
+      chunk_ids_list.extend((info_chunksy[num][0])) 
+      has_bird_list.extend((info_chunksy[num][3]))
+      representation_list.extend([repr_full_scaled[num][i] for i in range(len(i))])      
+      
+    testing_target = has_bird_list
+    
+    testing_features = np.array(representation_list)    
+    testing_features = np.reshape(testing_features, newshape=(testing_features.shape[0],-1))
+    
+    print("Test loaded")
+
+    return testing_features, testing_target
+
+def load_test_multitap(chosen_repr):
+    import pandas as pd
+    import os
+   
+    # datasets saved before (problems with library and python versions compatibility. Python 3.4 needed for library libtfr)
+    k_range = [3,6,9,12,15]
+    df_all = pd.read_hdf(os.path.join('..','data','df_test_repr3_0_norm.h5'),'df')
+    testy = df_all.loc[:,['rec_name', 'chunk_ids', chosen_repr, 'has_bird']].values
+    testy_identifiers_all = testy[:, 0:2]
+    test_X_all = np.stack(testy[:, 2])
+    test_y_all = testy[:, 3]
+    
+    for k in k_range:
+        df_k = pd.read_hdf(os.path.join('..','..','jupyter','data','df_test_repr3_'+ str(k) +'_norm.h5'),'df')
+        testy = df_k.loc[:,['rec_name', 'chunk_ids', chosen_repr, 'has_bird']].values
+        testy_identifiers = testy[:, 0:2]
+        test_X = np.stack(testy[:, 2])
+        test_y = testy[:, 3]        
+        print('stack: ', k)
+        test_X_all = np.vstack((test_X_all, test_X))
+        test_y_all = np.hstack((test_y_all, test_y))
+        testy_identifiers_all = np.vstack((testy_identifiers_all, testy_identifiers))
+    print(np.shape(test_X_all))   
+    print(np.shape(test_y_all))  
+    test_y_all = test_y_all.astype(int)
+    print(np.shape(testy_identifiers_all))  
+        
+    return np.array(test_X_all), np.array(test_y_all), np.array(testy_identifiers)
+
+def load_my_data_multitap(chosen_repr, config):
+
+    # datasets saved before (problems with library and python versions compatibility. Python 3.4 needed for library libtfr)
+    df1 = pd.read_hdf(os.path.join('..','..','jupyter','data','df_train_multitaper_norm.h5'),'df')
+    df2 = pd.read_hdf(os.path.join('..','..','jupyter','data','df_valid_multitaper_norm.h5'),'df')
+
+    cv_split, dX, dy, di, cv = make_cv_split_cnn(df1, df2, chosen_repr)    
+    print("Loaded train + valid  multitaper")
+    
+    valid = df2.loc[:,['rec_name', 'chunk_ids', chosen_repr, 'has_bird']].values
+    train = df1.loc[:,['rec_name', 'chunk_ids', chosen_repr, 'has_bird']].values
+    valid_X = np.stack(valid[:, 2])
+    valid_y = valid[:, 3]
+    train_X = np.stack(train[:, 2])
+    train_y = train[:, 3]
+    
+    return cv_split, dX, dy, di, cv, valid_X, valid_y, train_X, train_y 
+
